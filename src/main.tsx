4bdea0437f340844c4c4e29b820f2380f233a23d
Magowntown <amagown@gmail.com>
2023-07-12 13:38:09 -0400

    Tailwind + DaisyUI added

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import AboutPage from './pages/AboutPage.tsx'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />
+    },
+    {
+        path: "/about",
+        element: <AboutPage />
+    },
+        {
+        path: "/register",
+        element: <RegisterPage />
+    },
+        {
+        path: "/signin",
+        element: <SignInPage />
+    }
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
